baaa68b4f0de84fbeadd17f8af23b86f37f0a604
Tanvi <ravtanvi2006@gmail.com>
2025-10-04 10:36:41 +0530

    created file

---
+++ evenodd.py
@@ -0,0 +1,10 @@
+# Even or Odd Checker
+
+# Get input from user
+num = int(input("Enter a number: "))
+
+# Check even or odd
+if num % 2 == 0:
+    print(f"{num} is Even.")
+else:
+    print(f"{num} is Odd.")
